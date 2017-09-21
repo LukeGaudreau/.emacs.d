07cb888cac66a98b57bec47373a5b677aa9834ad
Luke Gaudreau <gaudreal@bc.edu>
2017-09-21 09:45:59 -0400

    Add evil-org

--- init.el
+++ init.el
@@ -247,6 +247,15 @@
 		       ("j" tags "+LEVEL=1+Jira")
 		       ("P" tags "Project")
 		       ))))))
+;; Evil Org
+(use-package evil-org
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+	    (lambda ()
+	      (evil-org-set-key-theme))))
 
 ;; Jira
 (use-package org-jira
